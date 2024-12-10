ebdb40ae34b4c94baf68aaaff7e28613b7c26e1b
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-12-10 14:57:59 +0100

    Added accessibilty features.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,6 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlaping red and white crosses, both straight and diagonally, on blue background",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe gold",
+        "US": "Flag with many red and white stripes, whit white stars in the top-left corner"
+    ]
+    
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
@@ -69,6 +84,7 @@ struct ContentView: View {
                             Image(countries[number])
                                 .modifier(FlagImage())
                         }
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         .rotation3DEffect(.degrees(tappedFlag == number ? animationAmount : 0.0), axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/)
                         .animation(.spring(duration: 1, bounce: 0.6), value: animationAmount)
                         .blur(radius: (tappedFlag != number ? blurDegree : 0.0))
